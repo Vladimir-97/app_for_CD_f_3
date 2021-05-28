84800e127bd7edc0975af1d40f92011243c9dde5
vova <vovlo97@gmail.com>
2021-05-28 09:29:30 +0500

    Invoice gataGridView

--- app_for_CD/UC_Invoice.cs
+++ app_for_CD/UC_Invoice.cs
@@ -8,18 +8,82 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Oracle.DataAccess.Client;
+using Microsoft.Office.Interop.Excel;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace app_for_CD
 {
     public partial class UC_Invoice : UserControl
     {
         OracleConnection con = null;
+
         public UC_Invoice()
         {
             InitializeComponent();
             SetConnection();
             LoadData();
         }
+        #region Для подключения excel
+
+        private string excelFilePath = string.Empty;
+        Excel.Application myExcelApplication;
+        Excel.Workbook myExcelWorkbook;
+        Excel.Worksheet myExcelWorkSheet;
+        
+
+        public void openExcel()
+        {
+            myExcelApplication = null;
+
+            myExcelApplication = new Excel.Application(); // create Excell App
+            myExcelApplication.DisplayAlerts = false; // turn off alerts
+
+
+            myExcelWorkbook = (Excel.Workbook)(myExcelApplication.Workbooks._Open(excelFilePath, System.Reflection.Missing.Value,
+               System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+               System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+               System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+               System.Reflection.Missing.Value, System.Reflection.Missing.Value)); // open the existing excel file
+
+            int numberOfWorkbooks = myExcelApplication.Workbooks.Count; // get number of workbooks (optional)
+
+            myExcelWorkSheet = (Excel.Worksheet)myExcelWorkbook.Worksheets[1]; // define in which worksheet, do you want to add data
+            myExcelWorkSheet.Name = "WorkSheet 1"; // define a name for the worksheet (optinal)
+
+            int numberOfSheets = myExcelWorkbook.Worksheets.Count; // get number of worksheets (optional)
+        }
+
+        public string ExcelFilePath
+        {
+            get { return excelFilePath; }
+            set { excelFilePath = value; }
+        }
+
+        public void closeExcel()
+        {
+            try
+            {
+                myExcelWorkbook.SaveAs(excelFilePath, System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                                               System.Reflection.Missing.Value, System.Reflection.Missing.Value, Excel.XlSaveAsAccessMode.xlNoChange,
+                                               System.Reflection.Missing.Value, System.Reflection.Missing.Value, System.Reflection.Missing.Value,
+                                               System.Reflection.Missing.Value, System.Reflection.Missing.Value); // Save data in excel
+
+
+                myExcelWorkbook.Close(true, excelFilePath, System.Reflection.Missing.Value); // close the worksheet
+
+
+            }
+            finally
+            {
+                if (myExcelApplication != null)
+                {
+                    myExcelApplication.Quit(); // close the excel application
+                }
+            }
+
+        }
+        #endregion
         private void SetConnection()
         {
             string ConnectionString = "USER ID=GGUZDR_APP;PASSWORD=gguzdr_app;DATA SOURCE=10.1.50.12:1521/GDBDRCT1";
@@ -46,7 +110,6 @@ namespace app_for_CD
             OracleDataReader dr = cmd.ExecuteReader();
             dr.Read();
             return Int32.Parse(dr[0].ToString());
-
         }
 
         private void LoadData()
@@ -96,6 +159,8 @@ namespace app_for_CD
                         dataGridView_invoice.Rows[i].Cells[9].Value = "Неактивный";
                     }
                     dataGridView_invoice.Rows[i].Cells[10].Value = dr[14];
+                    dataGridView_invoice.Rows[i].Cells[11].Value = dr[17];
+                    dataGridView_invoice.Rows[i].Cells[12].Value = dr[18];
                     i++;
                 }
                 else{
@@ -110,7 +175,7 @@ namespace app_for_CD
             
             for (int row = 0; row <= dataGridView_invoice.Rows.Count - 1; row++)
             {
-                ((DataGridViewImageCell)dataGridView_invoice.Rows[row].Cells[11]).Value = Properties.Resources.change;
+                ((DataGridViewImageCell)dataGridView_invoice.Rows[row].Cells[13]).Value = Properties.Resources.change;
             }
             
             dr.Close();
@@ -159,5 +224,46 @@ namespace app_for_CD
             
             }
         }
+
+        private void print_Click(object sender, EventArgs e)
+        {
+            excelFilePath = Path.GetFullPath("invoice_t.xlsx");
+            openExcel();
+            string val;
+            MessageBox.Show(myExcelWorkSheet.Cells[6, "B"].Text);
+            closeExcel();
+            /*
+            File.Copy("invoice_template.xlsx", "invoice_template-tmp.xlsx");
+            Excel.Application oXL;
+            Excel._Workbook oWB;
+            Excel._Worksheet oSheet;
+            try
+            {
+                //Start Excel and get Application object.
+                oXL = new Excel.Application();
+                oXL.Visible = true;
+
+                //Get a new workbook.
+                oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
+                oSheet = (Excel._Worksheet)oWB.ActiveSheet;
+                oSheet.Name = "Информация по договорам";
+                //Add table headers going cell by cell.
+               
+            }
+
+            catch (Exception theException)
+            {
+                String errorMessage;
+                errorMessage = "Error: ";
+                errorMessage = String.Concat(errorMessage, theException.Message);
+                errorMessage = String.Concat(errorMessage, " Line:  = ");
+                errorMessage = String.Concat(errorMessage, theException.Source);
+
+                MessageBox.Show(errorMessage, "Error");
+            }
+            */
+        }
+
+
     }
 }
